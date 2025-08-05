2525c9d1e57217d005c465fbb852035b0e6eabdf
saidee-hasan <mdsaideehasan@gmail.com>
2025-08-05 07:16:51 +0600

    problems

--- src/app/errorHelpers/handleDuplicateError.ts
+++ src/app/errorHelpers/handleDuplicateError.ts
@@ -1,10 +1,10 @@
-import { TError } from "../interfaces/globalErrorhandler";
+// import { TError } from "../interfaces/globalErrorhandler";
 
-export const handleDuplicateError = (error: any): TError => {
-  const matchedArray = error.errmsg.match(/"([^"]*)"/);
+// export const handleDuplicateError = (error: any): TError => {
+//   const matchedArray = error.errmsg.match(/"([^"]*)"/);
 
-  return {
-    statusCode: 400,
-    message: `${matchedArray[1]} already exist.`,
-  };
-};
+//   return {
+//     statusCode: 400,
+//     message: `${matchedArray[1]} already exist.`,
+//   };
+// };
